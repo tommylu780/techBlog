4d2e232ebabbb5b4e0e3458398e893657aed9133
tommylu780 <nhoksun340@gmail.com>
2021-06-13 17:51:23 +1000

    updated seeds

--- seeds/index.js
+++ seeds/index.js
@@ -1,10 +1,10 @@
-const seedUsers = require('./userData');
-const seedPosts = require('./postData');
-const seedComments = require('./commentData');
+const seedUsers = require('./user-seeds');
+const seedPosts = require('./post-seeds');
+const seedComments = require('./comment-seeds');
 
 const sequelize = require('../config/connection');
 
-const seedAll = async() => {
+const seedAll = async () => {
     await sequelize.sync({ force: true });
     console.log('\n----- DATABASE SYNCED -----\n');
     await seedUsers();
@@ -13,9 +13,9 @@ const seedAll = async() => {
     console.log('\n----- POSTS SEEDED -----\n');
     await seedComments();
     console.log('\n----- COMMENTS SEEDED -----\n');
-
-
+  
+  
     process.exit(0);
-};
-
-seedAll();
+  };
+  
+  seedAll();
